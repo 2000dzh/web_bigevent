45a4f81602ded247ab3fa0f24e0910f60f0c6984
yiquanyigeyinyingguai <2196714064@qq.com>
2021-08-14 19:55:37 +0800

    子模块部分提交

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,7 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
+   console.log( options.url);
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3008' + options.url;
 
@@ -15,13 +16,13 @@ $.ajaxPrefilter(function (options) {
     //全局同意挂载 complete函数
     // 无论成功还是失败，都会调用 complete函数
     options.complete = function (res) {
-        if (res.responseJSON.code === 1 && res.responseJSON.message === "身份认证失败！") {
+        if (res.responseJSON.code === 1) {
             //清空 token 数据 防止坏蛋手写一个token
             localStorage.removeItem('token');
             //强制跳回登录页面
             location.href = '/login.html'
         }
-        console.log(res);
+        // console.log(res);
     }
 
 })
